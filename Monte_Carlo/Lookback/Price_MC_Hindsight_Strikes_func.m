c316700c0ca081c2ff185393847d64bc84a6b157
Justin <jkirkby33@gmail.com>
2019-04-30 21:19:49 -0400

    Hindsight option Monte Carlo

---
+++ Monte_Carlo/Lookback/Price_MC_Hindsight_Strikes_func.m
@@ -0,0 +1,57 @@
+function [prices, stdErrs] = Price_MC_Hindsight_Strikes_func(Spath, call, strikes, M, mult, disc)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% About: Calculates Hindsight option prices for vector of strikes, given the simulatd paths 
+% NOTE: for hinsight options (fixed strike), returns an array of prices (or a single price if the length of strikes is 1)
+%     : for lookback options (floating strike), returns a single price
+%
+% Author: Justin Lars Kirkby
+%
+% -----------------
+% Params
+% -----------------
+% Contract Types
+% Fixed Strike (Hindsight) Put: (W - min{S_m: 0<=m<=M})^+
+% Fixed Strike (Hindsight) Call: (max{S_m: 0<=m<=M} - W)^+
+%
+% call = 1 for call (else put)
+% Kvec = strike vector
+% M = number of monitoring points, e.g. 252 for "daily" monitoring
+% mult = time partitioning multiplier to reduce bias (e.g. mult = 2 or 5)
+% S_0 = initial price
+% T = Time (in years)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+N_sim = size(Spath,1);  % number of paths
+
+M_mult = M*mult;  %time partitioning to reduce bias
+
+prices = zeros(length(strikes),1);
+stdErrs = zeros(length(strikes),1); %TODO: add code to return std errros
+
+if call == 1
+    curr_max = zeros(N_sim, 1);
+    for n = 1:N_sim
+        curr_max(n) = max(Spath(n, 1:mult:M_mult+1));
+    end
+else  % find_min
+    curr_min = zeros(N_sim, 1);
+    for n = 1:N_sim
+        curr_min(n) = min(Spath(n, 1:mult:M_mult+1));
+    end
+end
+
+for k = 1:length(strikes)
+    K = strikes(k);
+    if call ==1  % Fixed Strike (Hindsight) Call: (max{S_m: 0<=m<=M} - W)^+
+       payoffs  = max(0, curr_max - K);
+
+    else  % Fixed Strike (Hindsight) Put: (W - min{S_m: 0<=m<=M})^+
+        payoffs  = max(0, K - curr_min);
+    end
+    prices(k) = disc*mean(payoffs);
+    stdErrs(k) = disc*std(payoffs) / sqrt(N_sim);
+    
+end
+
+end
+
---
+++ Monte_Carlo/Lookback/Script_Hindsight_JumpDiffusion.m
@@ -0,0 +1,80 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% MONTE CARLO HINDSIGHT OPTION PRICER for Diffusions AND Jump Diffusions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Descritpion: Script to Price Hinsight options in Diffusion and Jump Diffusion Models
+%              using the Monte Carlo Simulation
+% Author:      Justin Kirkby
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[folder, name, ext] = fileparts(which( mfilename('fullpath')));
+cd(folder);
+
+addpath('../')
+
+% ---------------------
+%  Contract/Market Params
+% ---------------------
+call   = 1;    % For call use 1 (else, its a put)
+S_0    = 100;  % Initial price
+M      = 252;  % number of monitoring points, e.g. 252 for "daily" monitoring
+r      = 0.05;  % Interest rate
+q      = 0.00;  % dividend yield
+T      = 1;    % Time (in years)
+
+Kvec   = S_0*[.85 .90 .95 1 1.05 1.10 1.15 1.20 1.25 1.30 1.35 1.5 1.6];   % strikes to price
+
+
+% ---------------------
+% Model Params
+% ---------------------
+sigma = 0.2;  % diffusion parameter
+jumpModel = 0;  % determines jump model, select params below (set to 0 for no jumps)
+
+% ---------------------
+% Sim Params
+% ---------------------
+N_sim = 10^4;  % number of simulated paths
+mult = 2; % multiplier for simulation (see below) to reduce bias
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+jumpParams = {};
+
+if jumpModel == 1 %Normal Jumps, e.g. Merton
+    lambda = 1;  muJ = -.10;  sigJ = 0.3;
+    
+    jumpParams.kappa = exp(muJ + .5*sigJ^2)-1;  jumpParams.lambda = lambda; jumpParams.muJ = muJ; jumpParams.sigJ = sigJ;
+
+elseif jumpModel == 2 %Double Exponenial Jumps     
+    lambda = 1;
+    p_up   = 0.5; % up jump probability    
+    eta1   = 25;
+    eta2   = 30;
+    
+    kappa  = p_up*eta1/(eta1-1)+(1-p_up)*eta2/(eta2+1)-1;
+    jumpParams.lambda = lambda; jumpParams.kappa = kappa; jumpParams.eta1 = eta1; jumpParams.eta2 = eta2; jumpParams.p_up = p_up;    
+
+elseif jumpModel == 3 %Mixed normal Jumps
+    lambda = 1; 
+    a1 = -0.05; 
+    b1 = 0.07;    
+    a2 = 0.02; 
+    b2 = 0.03;
+    p_up = 0.6;
+
+    kappa = p_up*exp(a1 + .5*b1^2)+ (1-p_up)*exp(a2 + .5*b2^2)  -1;
+    jumpParams.lambda = lambda; jumpParams.kappa = kappa; jumpParams.a1 = a1; jumpParams.b1 = b1; jumpParams.a2 = a2; jumpParams.b2 = b2; jumpParams.p_up = p_up;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+M_mult = M*mult;  %time partitioning to reduce bias
+Spath = Simulate_Jump_Diffusion_func( N_sim, M_mult + 1, T, S_0, r, q, sigma, jumpModel, jumpParams);
+
+disc = exp(-r*T);
+[prices, stdErrs] = Price_MC_Hindsight_Strikes_func(Spath, call, Kvec, M, mult, disc)
+
+% Plot
+plot(Kvec, prices)
+ylabel('price')
+xlabel('strike')
+grid on;
